3006135439467dee70c145980e714f1fc63f0abc
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-07-11 20:44:21 +0300

    Test task

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from './components/Header/Header';
+import { Main } from './components/Main/Main';
+import './App.scss';
+
+const App: React.FC = () => (
+  <>
+    <Header />
+    <Main />
+    {/* <h1>Pagination</h1> */}
+    {/* <h1>Pagination</h1> */}
+  </>
+);
+
+export default App;
